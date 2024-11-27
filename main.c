c9e9fbfb5e673271ec2f13967bd6a417d071f649
carlopezc <carlotalcd@gmail.com>
2024-11-27 22:46:20 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carlopez <carlopez@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 17:53:54 by carlopez          #+#    #+#             */
+/*   Updated: 2024/11/27 21:23:53 by carlopez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int	fd;
+	char	*line;
+
+	fd = open("hola.txt", O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = NULL;
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
+}
